d99d282ed340861baf5d508c8516aa6fcfb7ac20
Eduard Gamiao <edgamiao@gmail.com>
2014-11-20 17:15:08 -1000

    Added (blank) login page.

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -10,5 +10,13 @@ public class Application extends Controller {
     public static Result index() {
         return ok(index.render("Your new application is ready."));
     }
+    
+    /**
+     * Renders the login page for the website.
+     * @return The Login view.
+     */
+    public static Result login() {
+      return ok(Login.render("Login"));
+    }
 
 }
