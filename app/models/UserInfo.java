0efdb12b070b69cd4dfb488b8f82726afa433782
Eduard Gamiao <edgamiao@gmail.com>
2014-12-05 00:06:07 -1000

    Added topic view for profile page.
    Cascade type required for models on the OneToMany relationship end.

--- app/models/UserInfo.java
+++ app/models/UserInfo.java
@@ -2,6 +2,7 @@ package models;
 
 import java.util.ArrayList;
 import java.util.List;
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
@@ -24,7 +25,7 @@ public class UserInfo extends Model {
   private String password;
   private String avatarURL;
   
-  @OneToMany
+  @OneToMany (mappedBy = "user", cascade = CascadeType.ALL)
   private List<Topic> topics = new ArrayList<Topic>();
   
   /**
